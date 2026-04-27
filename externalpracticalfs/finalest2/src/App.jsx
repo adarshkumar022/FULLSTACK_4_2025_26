bb95a5f0a2f87c3031997c460d1ab71cd207b4e7
adarshkumar022 <adarshmrx02@gmail.com>
2026-04-28 00:49:25 +0530

    Add files via upload

---
+++ externalpracticalfs/finalest2/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
